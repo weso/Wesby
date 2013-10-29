b7cdcb45eb3ed8eba1f717b8d496bf99a099e2e9
César Luis Alvargonzález <info@cesarla.com>
2013-10-29 08:13:50 +0100

    Modified Model and InverseModel

--- app/models/Node.scala
+++ app/models/Node.scala
@@ -5,39 +5,44 @@ import scala.collection.mutable.ListBuffer
 import com.hp.hpl.jena.rdf.model.{ Model => JenaModel, Property => JenaProperty, Resource => JenaResource, RDFNode => JenaRDFNode }
 
 sealed abstract class Node {
-  val rdfNode :  JenaRDFNode
+  val rdfNode: JenaRDFNode
 }
 
-case class Resource(val uri: String, 
-    val label: Option[String], 
-    rdfNode: JenaResource) extends Node {
-  
+case class Resource(
+  val uri: String,
+  val label: Option[String],
+  rdfNode: JenaResource) extends Node {
+
   def resource: JenaResource = rdfNode
-  
+
 }
-case class Property(val uri: String, 
-    val label: Option[String], 
-    rdfNode: JenaProperty) extends Node {
-  
-  def property : JenaProperty = rdfNode
-  
+case class Property(
+  val uri: String,
+  val label: Option[String],
+  rdfNode: JenaProperty) extends Node {
+
+  def property: JenaProperty = rdfNode
+
 }
-case class Literal(val value: String, 
-    val dataType: Option[String], 
-    rdfNode: JenaRDFNode) extends Node {
-  
+case class Literal(
+  val value: String,
+  val dataType: Option[String],
+  rdfNode: JenaRDFNode) extends Node {
+
   def literal: JenaRDFNode = rdfNode
-  
+
 }
 
 case class ResultQuery(subject: Model, predicate: InverseModel)
 
-case class Model(val jenaModel: JenaModel) extends DataStore {
-  
+case class Model(
+  val jenaModel: JenaModel,
+  val descendants: Option[Model] = None) extends DataStore {
+
   def add(p: Property, r: Resource) { addToDataStore(p, r) }
-  
+
   def add(p: Property, l: Literal) { addToDataStore(p, l) }
-  
+
   def add(p: Property, n: Node) { addToDataStore(p, n) }
 
   override def toString(): String = {
@@ -46,12 +51,14 @@ case class Model(val jenaModel: JenaModel) extends DataStore {
 
 }
 
-case class InverseModel(val jenaModel: JenaModel) extends DataStore {
+case class InverseModel(
+  val jenaModel: JenaModel,
+  val ascendants: Option[InverseModel] = None) extends DataStore {
 
   def add(r: Resource, p: Property) { addToDataStore(p, r) }
-  
+
   def add(l: Literal, p: Property) { addToDataStore(p, l) }
-  
+
   def add(n: Node, p: Property) { addToDataStore(p, n) }
 
   override def toString(): String = {
@@ -60,6 +67,7 @@ case class InverseModel(val jenaModel: JenaModel) extends DataStore {
 }
 
 trait DataStore {
+
   protected val map: HashMap[String, (Property, ListBuffer[Node])] = HashMap.empty
 
   protected def addToDataStore(p: Property, n: Node) {
