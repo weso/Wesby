69839c178d96d990853be78da063e2e61c1349af
Juan Castro Fernández <castrofernandez@gmail.com>
2013-11-01 15:45:32 +0100

    Dataset, indicator and observation custom view

--- app/es/weso/wfLodPortal/TemplateEngine.scala
+++ app/es/weso/wfLodPortal/TemplateEngine.scala
@@ -9,6 +9,9 @@ trait TemplateEgine extends Controller with Configurable {
   conf.append(new PropertiesConfiguration("conf/templates.properties"))
 
   protected val country = conf.getString("country.template")
+  protected val indicator = conf.getString("indicator.template")
+  protected val observation = conf.getString("observation.template")
+  protected val dataset = conf.getString("dataset.template")
 
   protected val RdfType = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
   protected val RdfLabel = "http://www.w3.org/2000/01/rdf-schema#label"
@@ -30,7 +33,10 @@ trait TemplateEgine extends Controller with Configurable {
       val r = typeResult.get    
       if (!r.nodes.isEmpty) {
        (r.nodes.head.node.asInstanceOf[models.RdfResource].uri.relative, 
-       		r.nodes.head.node.asInstanceOf[models.RdfResource].label getOrElse "")
+       		r.nodes.head.node.asInstanceOf[models.RdfResource].label 
+       			getOrElse 
+       			r.nodes.head.node.asInstanceOf[models.RdfResource].uri.short.get.prefix._2
+       			+ ":" + r.nodes.head.node.asInstanceOf[models.RdfResource].uri.short.get.suffix._2)
       } else ("", "")
     } else ("", "")
     
@@ -44,6 +50,9 @@ trait TemplateEgine extends Controller with Configurable {
     Ok(
       currentType match {
         case e if currentType == country => views.html.country(resultQuery, currentLabel, currentTypeUri)
+        case e if currentType == indicator => views.html.indicator(resultQuery, currentLabel, currentTypeUri)
+        case e if currentType == observation => views.html.observation(resultQuery, currentLabel, currentTypeUri)
+        case e if currentType == dataset => views.html.dataset(resultQuery, currentLabel, currentTypeUri)
         case _ => views.html.fallback(resultQuery, currentLabel, currentTypeUri)
 
       })
