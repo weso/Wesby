d372a7e1c8d48f0cc0ba6b430697d063b636a6fb
Jorge Yagüe París <jorgeyp@gmail.com>
2014-06-26 10:32:47 +0200

    Add mustache generation of properties with values.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,7 +13,7 @@ import play.api.mvc.Controller
 import play.api.mvc.RequestHeader
 import play.api.libs.ws.WS
 import play.api.libs.json._
-import es.weso.wesby.models.{Model, Options}
+import es.weso.wesby.models._
 import views.helpers.Utils._
 
 import scala.collection.mutable.ListBuffer
@@ -143,18 +143,12 @@ object Application extends Controller with TemplateEngine {
 
     subject.list.foreach(
       p => properties += Json.obj(
-        "property" -> Json.obj(
-          "uri" -> JsString(p.property.uri.relative),
-          "label" -> JsString(p.property.property.getLocalName)
-        ),
-        "value" -> {
+        "property" -> toJsonNode(p.property),
+        "values" -> {
           var nodes: ListBuffer[JsObject] = ListBuffer()
 
           p.nodes.foreach(
-            n => nodes += Json.obj(
-              "uri" -> JsString("node uri"),
-              "label" -> JsString("node label")
-            )
+            n => nodes += Json.obj("value" -> toJsonNode(n))
           )
           JsArray(nodes)
         }
@@ -162,6 +156,97 @@ object Application extends Controller with TemplateEngine {
     )
     JsArray(properties)
 
+  }
+
+  private def toJsonNode(n: RdfNode): JsObject = {
+
+    n match {
+      case r: RdfResource => resourceToUri(r)
+      case p: RdfProperty => propertyToUri(p)
+      case l: RdfLiteral => literalToValue(l)
+      case a: RdfAnon => {
+        Json.obj("value" -> JsString("1 anonymous resource " + a.rdfNode.getId))
+      }
+    }
+  }
+
+  def literalToValue(n: RdfLiteral): JsObject = {
+    n.dataType match {
+      case Some(dataType) => {
+        dataType.short match {
+          case Some(short) => {
+            Json.obj(
+              "literal" -> Json.obj(
+                "value" -> JsString(n.value),
+                "prefixUri" -> short.prefix._1,
+                "prefixLabel" -> short.prefix._2,
+                "suffixUri" -> short.suffix._1,
+                "suffixLabel" -> short.suffix._2
+              ))
+          }
+          case None => {
+            Json.obj("value" -> dataType.absolute)
+          }
+        }
+      }
+      case None => {
+        Json.obj("value" -> JsNull)
+      }
+    }
+  }
+
+  def resourceToUri(n: RdfResource): JsObject = {
+    cachedLabel(n).trim match {
+      case label if !label.isEmpty => {
+        Json.obj(
+          "label" -> showLabel(n.uri.absolute, label),
+          "uri" -> n.uri.relative
+        )
+      }
+      case _ => {
+        n.uri.short match {
+          case Some(short) => {
+            Json.obj(
+              "shortUri" -> Json.obj(
+                "prefixUri" -> short.prefix._1,
+                "prefixLabel" -> short.prefix._2,
+                "suffixUri" -> short.suffix._1,
+                "suffixLabel" -> short.suffix._2
+              ))
+          }
+          case None => {
+            Json.obj("label" -> n.uri.absolute)
+          }
+        }
+      }
+    }
+  }
+
+  def propertyToUri(n: RdfProperty): JsObject = {
+    cachedLabel(n).trim match {
+      case label if !label.isEmpty => {
+        Json.obj(
+          "label" -> showLabel(n.uri.absolute, label),
+          "uri" -> n.uri.relative
+        )
+      }
+      case _ => {
+        n.uri.short match {
+          case Some(short) => {
+            Json.obj(
+              "shortUri" -> Json.obj(
+                "prefixUri" -> short.prefix._1,
+                "prefixLabel" -> short.prefix._2,
+                "suffixUri" -> short.suffix._1,
+                "suffixLabel" -> short.suffix._2
+              ))
+          }
+          case None => {
+            Json.obj("none" -> n.uri.absolute)
+          }
+        }
+      }
+    }
   }
 
   /**
