2db7a707ad90f640e8caae9722dd44c4c252cf25
Jorge Yagüe París <jorgeyp@gmail.com>
2015-06-03 18:59:21 +0200

    Add new Play 2.4.0 project

---
+++ target/scala-2.11/routes/main/controllers/ReverseRoutes.scala
@@ -0,0 +1,46 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/jorge/Desarrollador/Wesby/conf/routes
+// @DATE:Wed Jun 03 18:45:58 CEST 2015
+
+import play.api.mvc.{ QueryStringBindable, PathBindable, Call, JavascriptLiteral }
+import play.core.routing.{ HandlerDef, ReverseRouteContext, queryString, dynamicString }
+
+
+import _root_.controllers.Assets.Asset
+
+// @LINE:6
+package controllers {
+
+  // @LINE:9
+  class ReverseAssets(_prefix: => String) {
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:9
+    def versioned(file:Asset): Call = {
+      implicit val _rrc = new ReverseRouteContext(Map(("path", "/public")))
+      Call("GET", _prefix + { _defaultPrefix } + "assets/" + implicitly[PathBindable[Asset]].unbind("file", file))
+    }
+  
+  }
+
+  // @LINE:6
+  class ReverseApplication(_prefix: => String) {
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:6
+    def index(): Call = {
+      import ReverseRouteContext.empty
+      Call("GET", _prefix)
+    }
+  
+  }
+
+
+}
---
+++ target/scala-2.11/routes/main/controllers/javascript/JavaScriptReverseRoutes.scala
@@ -0,0 +1,58 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/jorge/Desarrollador/Wesby/conf/routes
+// @DATE:Wed Jun 03 18:45:58 CEST 2015
+
+import play.api.routing.JavaScriptReverseRoute
+import play.api.mvc.{ QueryStringBindable, PathBindable, Call, JavascriptLiteral }
+import play.core.routing.{ HandlerDef, ReverseRouteContext, queryString, dynamicString }
+
+
+import _root_.controllers.Assets.Asset
+
+// @LINE:6
+package controllers.javascript {
+  import ReverseRouteContext.empty
+
+  // @LINE:9
+  class ReverseAssets(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:9
+    def versioned: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Assets.versioned",
+      """
+        function(file) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "assets/" + (""" + implicitly[PathBindable[Asset]].javascriptUnbind + """)("file", file)})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:6
+  class ReverseApplication(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:6
+    def index: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Application.index",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + """"})
+        }
+      """
+    )
+  
+  }
+
+
+}
