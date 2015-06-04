6dcd7fa64ac93a4dec60903cc72071881c49ec1d
Jorge Yagüe París <jorgeyp@gmail.com>
2015-06-04 18:23:07 +0200

    Add I18n support to Application controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,18 @@
 package controllers
 
-import play.api._
+import javax.inject.Inject
+
+import play.api.i18n.{Messages, I18nSupport, MessagesApi}
 import play.api.mvc._
 
-class Application extends Controller {
+class Application @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def welcome = Action { implicit request =>
+    Ok(Messages("welcome.test"))
   }
 
 }
---
+++ conf/messages.es
@@ -0,0 +1,1 @@
+welcome.test=Bienvenido a Wesby.
