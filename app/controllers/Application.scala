420e59a776f18671bf8485341f03625da0eef466
Jorge Yagüe París <jorgeyp@gmail.com>
2015-06-05 16:09:35 +0200

    Add several serialisation formats redirections

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,15 +11,17 @@ class Application @Inject() (val messagesApi: MessagesApi) extends Controller wi
 
   // Custom request extractors
   val AcceptsHtml = Accepting("text/html")
-  val AcceptsPlainText = Accepting("text/plain")
-  val AcceptsN3 = Accepting("text/n3")
-  val AcceptsRdfN3 = Accepting("text/rdf+n3")
   val AcceptsTurtle = Accepting("text/turtle")
+  val AcceptsNTriples = Accepting("application/n-triples")
+  val AcceptsJSONLD = Accepting("application/ld+json")
+  val AcceptsN3 = Accepting("text/n3")
+  val AcceptsRdfXML = Accepting("application/rdf+xml")
+  // TODO
+  val AcceptsPlainText = Accepting("text/plain")
+  val AcceptsRdfN3 = Accepting("text/rdf+n3")
   val AcceptsXTurtle = Accepting("application/x-turtle")
   val AcceptsXml = Accepting("application/xml")
-  val AcceptsRdfXML = Accepting("application/rdf+xml")
   val AcceptsRdfJSON = Accepting("application/rdf+json")
-  val AcceptsJSONLD = Accepting("application/ld+json")
 
   def index = Action { implicit request =>
     Redirect(Play.application().configuration().getString("wesby.index"))
@@ -33,14 +35,11 @@ class Application @Inject() (val messagesApi: MessagesApi) extends Controller wi
     Logger.debug("Dereferencing: " + path)
     render {
       case AcceptsHtml() => Redirect(request.path + ".html")
-      case AcceptsPlainText() => ???
-      case AcceptsJSONLD() => ???
+      case AcceptsTurtle() => Redirect(request.path + ".ttl")
+      case AcceptsNTriples() => Redirect(request.path + ".nt")
+      case AcceptsJSONLD() => Redirect(request.path + ".jsonld")
       case AcceptsN3() => Redirect(request.path + ".n3")
-      case AcceptsRdfN3() => ???
-      case AcceptsRdfXML() => ???
-      case AcceptsTurtle() => ???
-      case AcceptsXml() => Redirect(request.path + ".xml")
-      case AcceptsXTurtle() => ???
+      case AcceptsRdfXML() => Redirect(request.path + ".rdf")
     }
   }
 
