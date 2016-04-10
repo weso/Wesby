c3a4932e3e61bd1e9caa2403869116de999e2289
Jorge Yagüe París <jorgeyp@gmail.com>
2016-04-10 19:15:45 +0200

    Remove LDP tests to speed up testing process

--- test/LDPTestSuiteSpec.scala
+++ test/LDPTestSuiteSpec.scala
@@ -16,31 +16,31 @@ import play.api.test.Helpers._
 class LDPTestSuiteSpec extends Specification {
   // TODO LDP test suite
 
-  "Application" should {
-    "pass the LDP test suite" in new WithServer() {
-      // Default port: 19001
-
-      //configure test suite
-//      Logger.info("Running W3C official LDP Test Suite")
-//      val options = new util.HashMap[String, String]()
-//      options.put("server", "http://localhost:19001")
-
-//      val ldpTestSuite = new LdpTestSuite(options)
-//      ldpTestSuite.run()
-//      Logger.debug(ldpTestSuite.getStatus.toString)
-
-//      log.info("Running W3C official LDP Test Suite against '{}' server", baseUrl);
-//      Map<String, String> options = new HashMap<>();
-//      options.put("server", container);
-//      options.put("basic", null);
-//      options.put("non-rdf", null);
-//      options.put("cont-res", resource);
-//      if (!LdpService.SERVER_MANAGED_PROPERTIES.isEmpty()) {
-//        options.put("read-only-prop", LdpService.SERVER_MANAGED_PROPERTIES.iterator().next().stringValue());
-//      }
-//      options.put("httpLogging", null);
-//      options.put("skipLogging", null);
-//      options.put("excludedGroups", "MANUAL");
-    }
+//  "Application" should {
+//    "pass the LDP test suite" in new WithServer() {
+//      // Default port: 19001
+//
+//      //configure test suite
+////      Logger.info("Running W3C official LDP Test Suite")
+////      val options = new util.HashMap[String, String]()
+////      options.put("server", "http://localhost:19001")
+//
+////      val ldpTestSuite = new LdpTestSuite(options)
+////      ldpTestSuite.run()
+////      Logger.debug(ldpTestSuite.getStatus.toString)
+//
+////      log.info("Running W3C official LDP Test Suite against '{}' server", baseUrl);
+////      Map<String, String> options = new HashMap<>();
+////      options.put("server", container);
+////      options.put("basic", null);
+////      options.put("non-rdf", null);
+////      options.put("cont-res", resource);
+////      if (!LdpService.SERVER_MANAGED_PROPERTIES.isEmpty()) {
+////        options.put("read-only-prop", LdpService.SERVER_MANAGED_PROPERTIES.iterator().next().stringValue());
+////      }
+////      options.put("httpLogging", null);
+////      options.put("skipLogging", null);
+////      options.put("excludedGroups", "MANUAL");
+//    }
   }
 }
