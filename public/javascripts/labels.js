8df7585e40f6817e8206e95663d00a0fd42aad25
Jorge Yagüe París <jorgeyp@gmail.com>
2016-04-10 16:54:37 +0200

    Fix route in labels.js

--- public/javascripts/labels.js
+++ public/javascripts/labels.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         var uri = $this.find('a.value-uri').attr('href');
         var value = $this;
         // var labelDiv = $this.find('div.wesby-label');
-        $.get('wesby-label', {uri: uri})
+        $.get('/wesby-label', {uri: uri})
             .done(function (label) {
                 console.log(label);
                 value.html('<a href="' + uri + '">' + label + '</a>');
