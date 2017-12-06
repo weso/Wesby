068333458f539dc1dc6ecb0dc185d064f8101ecc
Jorge Yagüe París <jorgeyp@gmail.com>
2017-12-06 20:28:07 +0100

    Add more symbols to keyboard

--- public/javascripts/keyboard.js
+++ public/javascripts/keyboard.js
@@ -4,20 +4,40 @@ $(function () {
         customLayout: {
             'default' : [
                 // "n(a):title_or_tooltip"; n = new key, (a) = actual key, ":label" = title_or_tooltip (use an underscore "_" in place of a space " ")
-                '\u0101 \u010D \u1E0D \u1E0F \u0113 \u01E7',
-                '\u0121 \u1E25 \u1E96 \u1E2B \u0127 \u012B',
-                '\u01F0 \u1E33 \u014D \u1E63 \u0161 \u1E6D',
-                '\u1E6F \u016B \u0177 \u1E93 \u1E95 \u1D9C',
-                '{shift} {accept} {cancel}'
+                // a c c d d e
+                '\u0101 \u010D \u010B \u1E0D \u1E0F \u0113',
+                // g g h h h h
+                '\u01E7 \u0121 \u1E25 \u1E96 \u1E2B \u0127',
+                // i j k o s s
+                '\u012B \u01F0 \u1E33 \u014D \u1E63 \u0161',
+                // t t u y z z
+                '\u1E6D \u1E6F \u016B \u0177 \u1E93 \u1E95',
+                '{shift} {alt} {accept} {cancel}'
             ],
             'shift' : [
-                '\u0100 \u010C \u1E0C \u1E0E \u0112 \u01E6',
-                '\u0120 \u1E24 H\u0331 \u1E2A \u0126 \u012A',
-                'J\u030C \u1E32 \u014C \u1E62 \u0160 \u1E6C',
-                '\u1E6E \u016A \u0176 \u1E92 \u1E94 \u1D9C',
-                '{shift} {accept} {cancel}'
+                // a c _ d d e
+                '\u0100 \u010C {sp:1} \u1E0C \u1E0E \u0112',
+                // g g h h h h
+                '\u01E6 \u0120 \u1E24 H\u0331 \u1E2A \u0126',
+                // i j k o s s
+                '\u012A J\u030C \u1E32 \u014C \u1E62 \u0160',
+                // t t u y z z
+                '\u1E6C \u1E6E \u016A \u0176 \u1E92 \u1E94',
+                '{shift} {alt} {accept} {cancel}'
+            ],
+            'alt' : [
+                // a c h i
+                '\u1D43 \u1D9C \u02B0 \u1DA5' ,
+                // i n t u
+                '\u02E1 \u207F \u1D57 \u1D58',
+                '{shift} {alt} {accept} {cancel}'
             ]
         },
-        usePreview: false
+        usePreview: false,
+        display: {
+            'accept': 'Buscar',
+            'shift': 'Mayus',
+            'cancel': 'Cancelar'
+        }
     });
 });
